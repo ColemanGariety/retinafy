96a38d2c1ff7b15e37d4943459d2888d3219ae63
JacksonGariety <business@jacksongariety.com>
2012-06-22 00:26:58 -0700

    Fixes background image retinification and adds demo. Also fixes typos on the demo page.

--- retinafy.js
+++ retinafy.js
@@ -10,12 +10,20 @@
 					
 						for (i=0; i<retina.length; i++) {
 							var img = retina[i];
-								var imgWidth = $(img).width();
-								var imgHeight = $(img).height();
-								var srcImg = $(img).attr('src');
-								var bgImg = $(img).css('background-image');
-								
-								function urlExists(url) {
+							var srcImg = $(img).attr('src');
+							var bgImg = $(img).css('background-image');
+							
+							function IsImageOk(img) {
+							    if (!img.complete) {
+							        return false;
+							    }
+							    if (typeof img.naturalWidth != "undefined" && img.naturalWidth == 0) {
+							        return false;
+							    }
+							    return true;
+							}
+							
+							function urlExists(url) {
 									if (!(!url.match(/^https?\:/i) || url.match("//" + document.domain))) {
 										return false;
 									} else {
@@ -25,20 +33,34 @@
 									    return http.status != 404;
 									}
 								}
+							
+							function doRetina() {
+								var imgWidth = $(img).width();
+								var imgHeight = $(img).height();
 								
 								if ($(img).is('img') && (srcImg.indexOf('@2x') == -1)) {
+									
 									var stry = srcImg.lastIndexOf('.');
 									if (stry != -1) {
 										srcImg = srcImg.substr(0, stry) + "@2x" + srcImg.substr(stry);
 									}
 									
 									if (urlExists(srcImg)) {
-											$(img).attr('src',srcImg);
+										$(img).attr('src',srcImg);
 										$(img).attr('width',imgWidth).height('height',imgHeight);
 									}
 								}
-								
-								else if (bgImg !== 'none' && (bgImg.indexOf('@2x') == -1)) {
+							}
+							if (IsImageOk(img)) {
+								doRetina()
+							} else {
+								if ($(img).is('img') && (srcImg.indexOf('@2x') == -1)) {
+									img.onload = function(){
+										doRetina();
+									}
+								} else {
+									var imgWidth = $(img).width();
+									var imgHeight = $(img).height();
 									var strx = bgImg;
 									var stry = strx.lastIndexOf('.');
 									
@@ -52,11 +74,12 @@
 									
 									if (urlExists(bgImgUrl)) {
 										$(img).css('background-image',strx);
-									}
+										$(img).css('background-size',imgWidth + 'px ,' + imgHeight + 'px');
+									};
 								}
-						}
-				
-				}
+							}
+						};
+				};
 				
 			return true;
 				
